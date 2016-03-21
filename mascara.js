8806d93625530d405514d2c19a2a2e785da4af81
FernandoHV <fernandohvictor@hotmail.com>
2016-03-21 11:21:27 -0300

    Update mascara.js

--- mascara.js
+++ mascara.js
@@ -1,38 +1,45 @@
 
-jQuery.fn.mascarar = function() {
+$.fn.mascararTelefone = function() {
 
     var nums = Array();
-
-    $(this).on('keypress', function(e) {
-		var code = e.keyCode || e.which
-        if (code != 8 && isNaN(String.fromCharCode(code)) || code == 32) {
+    var input = $(this);
+    
+    console.info(input);
+    
+    input.on('keypress', function(e) {
+	var code = e.keyCode || e.which;
+        if (code !== 8 && isNaN(String.fromCharCode(code)) || code === 32) {
             e.preventDefault(); //se nao for numero para o codido
         }
-        if ($(this).val().length < 15) {
+        if (input.val().length < 15) {
 
-            var atual = $(this).val();
-            var qtd = $(this).val().length;
+            var atual = input.val();
+            var qtd = input.val().length;
 
-            if ($(this).val() != "") {
+            if (input.val() !== "") {
 
-                if (qtd == 1 && code != 8) {
-                    $(this).val('(' + atual);
+                if (qtd === 1 && code !== 8) {
+                     input.val('(' + atual);
                 }
-                if (qtd == 3 && code != 8) {
-                    $(this).val(atual + ') ');
+                if (qtd === 3 && code !== 8) {
+                    input.val(atual + ') ');
                 }
-                if (qtd == 9 && code != 8) {
-                    $(this).val(atual + '-');
+                if (qtd === 9 && code !== 8) {
+                    input.val(atual + '-');
+                }
+                
+                if(input.val().indexOf("((") >= 0){
+                   input.val(input.val().replace("((","("));
                 }
 
-                if (qtd == 14) {
-                    if (code != 8 && isNaN(String.fromCharCode(code)) || code == 32) {
+                if (qtd === 14) {
+                    if (code !== 8 && isNaN(String.fromCharCode(code)) || code === 32) {
                         return false;
                     }
                     for (i = 0; i < qtd; i++) {
-                        nums[i] = atual.substr(i, 1)
+                        nums[i] = atual.substr(i, 1);
                     }
-                    $(this).val(nums[0] + nums[1] + nums[2] + nums[3] + nums[4] + nums[5] + nums[6] + nums[7] + nums[8] + nums[10] + nums[9] + nums[11] + nums[12] + nums[13]);
+                    input.val(nums[0] + nums[1] + nums[2] + nums[3] + nums[4] + nums[5] + nums[6] + nums[7] + nums[8] + nums[10] + nums[9] + nums[11] + nums[12] + nums[13]);
                 }
 
 
@@ -40,23 +47,23 @@ jQuery.fn.mascarar = function() {
             }
 
         } else {
-			 if (code != 8){
+			 if (code !== 8){
             return false;
 			 }
         }
 
     });
 
-    $(this).on('keyup', function(e) {
-		var code = e.keyCode || e.which
+    input.on('keyup', function(e) {
+		var code = e.keyCode || e.which;
 		
-        if (code == 8) {
-            if ($(this).val().length == 14) {
-                for (i = 0; i < $(this).val().length; i++) {
-                    nums[i] = $(this).val().substr(i, 1)
+        if (code === 8) {
+            if (input.val().length === 14) {
+                for (i = 0; i < input.val().length; i++) {
+                    nums[i] = input.val().substr(i, 1);
                 }
 
-                $(this).val(nums[0] + nums[1] + nums[2] + nums[3] + nums[4] + nums[5] + nums[6] + nums[7] + nums[8] + nums[10] + nums[9] + nums[11] + nums[12] + nums[13]);
+                input.val(nums[0] + nums[1] + nums[2] + nums[3] + nums[4] + nums[5] + nums[6] + nums[7] + nums[8] + nums[10] + nums[9] + nums[11] + nums[12] + nums[13]);
 
 
             }
